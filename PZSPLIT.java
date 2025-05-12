cf29539b714fc2e0d6d009a9542fbd7e406e2624
NIKHILSAHU12 <116480931+NIKHILSAHU12@users.noreply.github.com>
2025-05-12 21:07:59 +0530

    Add files via upload
    Chef and Chefina are ordering pizza.
    Each pizza has N slices.
    what is the minimum number of pizza they must order so that both can eat equal number of slices?
    
    If input - 6 then output will be 1.
    If input - 3 then output will be 2.  
    
    Solution
    For that we will have an if condition where modulo operation will be applied on input. N%2==0 then we will print 1.
    else print 2.

---
+++ PZSPLIT.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner ob = new Scanner (System.in);
+		int N = ob.nextInt();
+		if (N%2==0)
+		{
+		    System.out.println("1");
+		}
+		else
+		{
+		    System.out.println("2");
+		}
+
+	}
+}
